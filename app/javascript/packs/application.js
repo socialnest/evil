979fdbffffd52f0c4d44eca6e07bcaf1676e8bb4
0x44L <rapid6880@gmail.com>
2022-09-15 10:47:34 +0000

    first commit

---
+++ app/javascript/packs/application.js
@@ -0,0 +1,15 @@
+// This file is automatically compiled by Webpack, along with any other files
+// present in this directory. You're encouraged to place your actual application logic in
+// a relevant structure within app/javascript and only use these pack files to reference
+// that code so it'll be compiled.
+import 'bootstrap/dist/js/bootstrap.bundle.min'
+import 'bootstrap/dist/css/bootstrap.min'
+import '../stylesheets/application'
+import Rails from "@rails/ujs"
+import Turbolinks from "turbolinks"
+import * as ActiveStorage from "@rails/activestorage"
+import "channels"
+require("packs/jquery-3.4.1.min")
+Rails.start()
+Turbolinks.start()
+ActiveStorage.start()
